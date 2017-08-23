b41474d33b799ecf93911ff86dadf13087bcdcae
shiva kiran <shiva.sindham@wavemaker.com>
2017-08-23 06:32:51 +0000

    f New files added - [fonts,material,material] Files modified - [data,webapp,.wmproject.properties] Removed files - [cool-blue,fonts]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,5 +1,5 @@
 var _WM_APP_PROPERTIES = {
-  "activeTheme" : "cool-blue",
+  "activeTheme" : "material",
   "defaultLanguage" : "en",
   "displayName" : "SalesVision_juLy2017",
   "homePage" : "Main",
